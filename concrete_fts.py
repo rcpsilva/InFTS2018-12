953c768ca294c6dba0b113c151f90ea4222f5947
rcpsilva <rcpsilva@gmail.com>
2018-12-10 23:12:03 -0200

    High-order FTS
    Still lacks defuzification

--- concrete_fts.py
+++ concrete_fts.py
@@ -1,16 +1,18 @@
-import FTS
+from FTS import FTS
 import rule_base_management as rbm
 import partition_utilities as pu
 import pertinence_funcs as pf
 import numpy as np
 
 
-class ConcreteFTS:
+class ConcreteFTS(FTS):
 
     def __init__(self, nsets, order, lb, ub):
 
         self.nsets = nsets
         self.order = order
+        self.lb = lb
+        self.ub = ub
         self.fuzzy_sets = np.arange(self.nsets)
         self.rule_base = rbm.init_rule_base(self.fuzzy_sets, self.order)
         self.window = []  # Stores the last "order" data
@@ -18,27 +20,30 @@ class ConcreteFTS:
 
     def fit(self, data):
 
-        # Get pertinences
-        pertinence_list = []
+        for i in range(len(data)-self.order):
+            window = data[i:(i+self.order+1)]
 
-        for x in data:
-            pertinence = []
-            for p in self.partitions:
-                pertinence.append(pf.t_pertinence(x, p))
-            pertinence_list.append(pertinence)
+            # Get pertinences
+            pertinence_list = []
 
-        # Build rule
-        rule = rbm.generate_rule(pertinence_list)
+            for x in window:
+                pertinence = []
+                for p in self.partitions:
+                    pertinence.append(pf.t_pertinence(x, p))
+                pertinence_list.append(pertinence)
 
-        # Add rule to the rule base
+            # Build rule
+            rule = rbm.generate_rule(pertinence_list)
 
-        rbm.add_rule(self.rule_base, rule, self.nsets, self.order)
+            # Add rule to the rule base
+
+            rbm.add_rule(self.rule_base, rule, self.nsets, self.order)
 
     def predict(self):
         pass
 
 
-fts = ConcreteFTS(3, 1, 1, 4)
+fts = ConcreteFTS(3, 3, 0, 3)
 print(fts.partitions)
-fts.fit([1, 2])
+fts.fit([0, 1, 2, 0, 1, 2])
 rbm.print_rule_base(fts.rule_base)
